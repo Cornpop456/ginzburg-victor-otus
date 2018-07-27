8043cf3ac32fbc5a03468cd580430db9152eafc2
Viktor Ginzburg <v-viktor456@mail.ru>
2018-07-27 14:56:16 +0300

    Update sum.js

--- javascript/sum.js
+++ javascript/sum.js
@@ -1,6 +1,6 @@
 function sum(value) {
 	function inner(value) {
-		if (!value) return inner.sum || 'Нет переданных значений';
+		if (value === undefined) return inner.sum || 'Нет переданных значений';
 		if (!inner.sum) inner.sum = 0;
 
 		inner.sum += value;
