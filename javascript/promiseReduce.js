c9dcf356cc5740880e91ff83015f1dc337753276
Victor Ginzburg <v-viktor456@mail.ru>
2018-08-01 19:18:48 +0300

    promiseReduce completed

---
+++ javascript/promiseReduce.js
@@ -0,0 +1,21 @@
+function promiseReduce(asyncFunctions, reduce, initialValue) {
+	let accum = initialValue;
+
+	return new Promise((resolve, reject) => {
+		const processFunctions = (fns) {
+			if (fns.length === 0) return resolve(accum);
+
+			let currentFn = fns[0];
+			fns = fns.slice(1);
+			
+			return currentFn()
+				.then(res => {
+					accum = reduce(accum, res);
+					return fns;
+				})
+				.then(processFunctions);
+		}
+
+		processFunctions(asyncFunctions);
+	});
+}
