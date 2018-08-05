fe8e05e2b1e9d4cf1ac44a9bb3ecfee010423f01
Viktor Ginzburg <v-viktor456@mail.ru>
2018-08-05 13:30:43 +0300

    Update promiseReduce.js

--- javascript/promiseReduce.js
+++ javascript/promiseReduce.js
@@ -1,10 +1,8 @@
 function promiseReduce(asyncFunctions, reduce, initialValue) {
     return asyncFunctions.reduce((accum, current) => {
-        //при первом запуске, accum не является промисом
-        if (!accum.then) accum = Promise.resolve(accum);
         return accum
             .then(accumValue => Promise.all([accumValue, current()]))
             .then(([accumValue, currentValue]) => reduce(accumValue, currentValue))
             .catch(err => { throw err });
-    }, initialValue);
+    }, Promise.resolve(initialValue));
 }
