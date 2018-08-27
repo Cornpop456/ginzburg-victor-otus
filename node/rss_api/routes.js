a883d16dd8f19ce76ab4b908eefb802584434580
Victor Ginzburg <v-viktor456@mail.ru>
2018-08-27 15:50:40 +0300

    fixes

--- node/rss_api/routes.js
+++ node/rss_api/routes.js
@@ -4,7 +4,7 @@ const grab = require('./grab');
 const saveFeed = function(req, res) {
   const { rss_url } = req.body;
 
-  if (typeof rss_url === 'undefined') {
+  if (!rss_url) {
     return res.json({ error: 'Не передан url', done: false });
   }
 
@@ -12,26 +12,20 @@ const saveFeed = function(req, res) {
     .then(count => {
       res.json({ result: { newsAdded: count }, done: true });
     })
-    .catch(err => {
-      res.json({ error: err.message, done: false });
-    });
+    .catch(err => res.json({ error: err.message, done: false }));
 };
 
 const getFeedUrls = function(req, res) {
   Feed.find()
     .distinct('rssUrl')
     .then(arr => res.json({ result: arr, done: true }))
-    .catch(err => {
-      res.json({ error: err.message, done: false });
-    });
+    .catch(err => res.json({ error: err.message, done: false }));
 };
 
 const getAllData = function(req, res) {
   Feed.find()
-    .then(arr => res.send(JSON.stringify({ result: arr, done: true }, null, 4)))
-    .catch(err => {
-      res.json({ error: err.message, done: false });
-    });
+    .then(arr => res.json({ result: arr, done: true }))
+    .catch(err => res.json({ error: err.message, done: false }));
 };
 
 module.exports = { saveFeed, getFeedUrls, getAllData };
