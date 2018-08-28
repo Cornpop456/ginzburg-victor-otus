a79974e0f7b409ed0b1b6245273da6e1a58be582
Victor Ginzburg <v-viktor456@mail.ru>
2018-08-28 09:35:18 +0300

    refactoring

--- javascript/getPath.js
+++ javascript/getPath.js
@@ -1,25 +1,22 @@
 function getPath(node, path = '') {
-  if (node.tagName === 'BODY') return path.slice(0, -1);
-
-  const initialNode = node;
-
-  if (node.id) {
+  if (node.tagName === 'BODY') {
+    return path.slice(0, -1);
+  } else if (node.id) {
     path = '#' + node.id + '>' + path;
     return path.slice(0, -1);
-  } else {
-    let pos = 1;
-    while ((node = node.previousElementSibling) != null) pos++;
-
-    const sameTags = Array.from(initialNode.parentNode.children)
-      .map(elem => elem.tagName)
-      .filter(elem => elem === initialNode.tagName);
-
-    if (sameTags.length === 1) {
-      path = initialNode.tagName.toLocaleLowerCase() + '>' + path;
-    } else {
-      path = initialNode.tagName.toLocaleLowerCase() + `:nth-child(${pos})>` + path;
-    }
-
-    return getPath(initialNode.parentElement, path);
   }
+
+  const nodeTagName = node.tagName.toLocaleLowerCase(),
+    sameTags = Array.from(node.parentNode.children).filter(elem => elem.tagName === node.tagName);
+
+  if (sameTags.length === 1) {
+    path = nodeTagName + '>' + path;
+  } else {
+    let tempNode = node,
+      pos = 1;
+    while ((tempNode = tempNode.previousElementSibling) !== null) pos++;
+    path = nodeTagName + `:nth-child(${pos})>` + path;
+  }
+
+  return getPath(node.parentElement, path);
 }
